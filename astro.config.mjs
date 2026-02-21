df9b5addf06ff3a27449508aceb2c3f0a6051fbc
Alberto Barredo <alber@MacBook-Pro-de-Alberto.local>
2026-02-21 18:53:32 +0100

    Fix invalid Astro markup breaking production

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,28 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     drafts: true,
     shikiConfig: {
       theme: "poimandres"
     }
   },
+
   shikiConfig: {
     wrap: true,
     skipInline: false,
     drafts: true
   },
+
   site: 'https://yourdomain.com',
-  integrations: [sitemap(), mdx()]
+  integrations: [sitemap(), mdx()],
+  adapter: cloudflare()
 });
